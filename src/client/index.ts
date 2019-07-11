eb61602ff2d0e2f107b5a51c34691e920acb981f
davidn <giangnn@gmail.com>
2019-07-11 20:09:51 +1000

    update boilerplate

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,8 @@
-window.document.write("hello from js");
+import Axios from 'axios';
+
+const btn = document.getElementById('foo');
+btn.addEventListener('click', () => {
+    Axios.post('/foo', { value: 'bar' })
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
+});
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,21 @@
 // that will be called once the module is reloaded
 
 import * as express from 'express';
+import { json, urlencoded } from 'body-parser';
+import * as cors from 'cors';
 import {Request, Response} from 'express';
 
 const app = express();
 app.use(express.static('dist/public'));
+app.use(json());
+app.use(cors());
+app.use(urlencoded({ extended: false }));
+
+app.post('/foo', (req, res) => {
+	req.body.value = 'foo';
+	res.send(req.body);
+});
+
 var server = app.listen(8080, () => console.log('Listening on port 8080'));
 
 export function cleanup(){
